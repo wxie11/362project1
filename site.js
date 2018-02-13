dede96c8b819764e7de1858d8c6c31268b4b6c51
Wenwen Xie <wxie11@hawk.iie.edu>
2018-02-12 22:00:47 -0600

    add form validation

--- site.js
+++ site.js
@@ -2,8 +2,29 @@ $('#info-form').on("submit",
   function(e) {
     var email = $('#email').val();
     var name = $('#name').val();
-    var valid_email = /.+@.+/;
+    var phone = $('#phone').val();
+    var birthday = $('#birthday').val();
+    var valid_name = /^[a-zA-Z\s]+$/;
+    var valid_email = /^[^\s@]+@[^\s@]+$/;
+    var valid_phone = /^(\d{10})*\d{10}$/;
     e.preventDefault();
+
+    if(!valid_name.test(name))
+    {
+      console.log('invalid name');
+      $('.invalid').remove();
+      $('#name-list').append('<p class="invalid">Please enter your first name and last name with only letters.</p>');
+      return false;
+    }
+
+    if(birthday == "")
+    {
+      console.log('invalid date');
+      $('.invalid').remove();
+      $('#birthday-list').append('<p class="invalid">Please enter your birthday.</p>');
+      return false;
+    }
+
     if(!valid_email.test(email))
     {
       console.log('invalid email');
@@ -11,13 +32,23 @@ $('#info-form').on("submit",
       $('#email-list').append('<p class="invalid">Please enter a valid email address.</p>');
       return false;
     }
+
+    if(!valid_phone.test(phone))
+    {
+      console.log('invalid phone number');
+      $('.invalid').remove();
+      $('#phone-list').append('<p class="invalid">Please enter a valid phone number.</p>');
+      return false;
+    }
     else
     {
       $(this).remove();
-      $('h2').remove();
-      $('section').append('<h2>Thank you!</h2>')
-      $('section').append('<h1>'+ name +'</h1>');
-      $('section').append('<p>Your form has been submitted successfully!</p>');
+      $('#form-heading').remove();
+      $('#form-area').append('<h1>Thank you!</h1>')
+      $('#form-area').append('<h1>'+ name +'</h1>');
+      $('#form-area').append('<li>Your email: '+ email +'</li>');
+      $('#form-area').append('<li>Your phone: '+ phone +'</li>');
+      $('#form-area').append('<li>Your form has been submitted successfully!</li>');
     }
   }
 );
